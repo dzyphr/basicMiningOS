89eb99bf7f887f6fbc18089f86a9ef1b00866c68
justaguy12345 <ryanberk135@gmail.com>
2024-09-16 22:28:47 -0500

    update nvidiaPack unnote scripts

--- nvidiaPack.sh
+++ nvidiaPack.sh
@@ -1,6 +1,6 @@
-#sudo apt-get install gpustat
-#wget -nc https://github.com/develsoftware/GMinerRelease/releases/download/3.44/gminer_3_44_linux64.tar.xz
-#tar -xvf gminer_3_44_linux64.tar.xz #TODO modularize the miner choice obviously
+sudo apt-get install gpustat
+wget -nc https://github.com/develsoftware/GMinerRelease/releases/download/3.44/gminer_3_44_linux64.tar.xz
+tar -xvf gminer_3_44_linux64.tar.xz #TODO modularize the miner choice obviously
 echo "What coin do you want to mine?
 options: [Ergo]"
 read coinoption
